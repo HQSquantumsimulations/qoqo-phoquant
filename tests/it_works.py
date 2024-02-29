3a13909c750f390465d08cfe494c04344701c510
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2024-02-29 11:55:14 +0100

    Trying to fix workflow

--- tests/it_works.py
+++ tests/it_works.py
@@ -1,5 +1,6 @@
 """"Dummy test file to test the workflows."""
+# Copyright © 2024 HQS Quantum Simulations GmbH. All Rights Reserved.
 
-
-def test_dummy():
+def test_dummy() -> None:
+    """Dummy test."""
     assert 2 + 2 == 4
